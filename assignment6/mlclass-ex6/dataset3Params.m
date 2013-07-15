f9bbd7b311ca8838cd3006b0d4200289ddc61cb8
bjing <brianjing404@gmail.com>
2013-07-16 00:06:08 +1000

    Answers for the first half of the exercise

--- assignment6/mlclass-ex6/dataset3Params.m
+++ assignment6/mlclass-ex6/dataset3Params.m
@@ -23,9 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+candidates = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+combos = zeros(length(candidates)^2, 3);
+
+min_error = 100000;
+for i = 1:length(candidates)
+    for j = 1:length(candidates)
+        % Values of C and sigma to try
+        C = candidates(i);
+        sigma = candidates(j);
+        % Train model
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        % Make predicton
+        predictions = svmPredict(model, Xval);
+        % Calculate error
+        error_cv = mean(double(predictions ~= yval))
+        % Check whether C and sigma values are better
+        if error_cv < min_error
+            min_error = error_cv;
+            best_c = C;
+            best_sigma = sigma;
+        endif
+    endfor
+endfor
 
 
-
+C = best_c;
+sigma = best_sigma;
 
 
 
